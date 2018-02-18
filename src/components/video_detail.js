82dc718860722bc842ba310bd4d33bca5f4a6d6a
Joe Park-Kennaby <jparkkennaby@gmail.com>
2018-02-18 22:57:57 +0000

    finish building youtube app

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-const App =  () => {
-  return <div>Hi!</div>;
+
+const VideoDetail =  ({video}) => {
+
+  if (!video) {
+    return <div>Loading..</div>;
+  }
+  
+const videoId = video.id.videoId;
+const url = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url}></iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
 }
 
- ReactDOM.render(<App />, document.querySelector('.container'));
+export default VideoDetail;
